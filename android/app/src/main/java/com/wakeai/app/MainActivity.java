37d7847dac2e4015d4d14e7b3225461766cdcd6a
Mohammad Anas <anas.1633.m@gmail.com>
2026-02-10 11:13:01 +0530

    fix: MainActivity access modifier for onDestroy()

--- android/app/src/main/java/com/wakeai/app/MainActivity.java
+++ android/app/src/main/java/com/wakeai/app/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         releaseWakeLock();
         super.onDestroy();
     }
